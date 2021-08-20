eaf13b91906d5d7b4e993a2a29e8a8869a5dc9b0
Tan Huynh <huynhminhtan4019@gmail.com>
2021-08-20 23:27:01 +0700

    render + parti + encode

--- src/app.js
+++ src/app.js
@@ -1,24 +1,23 @@
-const express = require('express')
-const morgan = require('morgan')
-const exphbs  = require('express-handlebars')
-const path = require('path')
-const app = express()
-const port = 3000
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const app = express();
+const port = 3000;
 
 //morgan
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 //express-handlebars
-app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.engine('.hbs', exphbs({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
-app.set('views',path.join(__dirname, '/resources/views'))
+app.set('views', path.join(__dirname, '/resources/views'));
 
 app.get('/', (req, res) => {
   res.render('home');
-})
-
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
