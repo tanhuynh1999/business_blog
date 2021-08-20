d5dce77f980cb8d29a174f917dcd7857cc520042
Tan Huynh <huynhminhtan4019@gmail.com>
2021-08-20 23:07:02 +0700

    all

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const morgan = require('morgan')
+const exphbs  = require('express-handlebars')
+const path = require('path')
+const app = express()
+const port = 3000
+
+//morgan
+app.use(morgan('combined'))
+
+//express-handlebars
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+app.set('views',path.join(__dirname, '/resources/views'))
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
